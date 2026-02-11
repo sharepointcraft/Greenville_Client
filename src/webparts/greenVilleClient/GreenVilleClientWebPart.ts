608ec8019f43e0bf4bed92ad459317b2f3ee86d8
sharepointcraft <sharepointcraft@gmail.com>
2026-02-11 18:24:24 +0530

    Add GreenVilleClient web part with initial structure and styles (First Commit)

---
+++ src/webparts/greenVilleClient/GreenVilleClientWebPart.ts
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IReadonlyTheme } from '@microsoft/sp-component-base';
+
+import GreenVilleClient from './components/GreenVilleClient';
+import { IGreenVilleClientProps } from './components/IGreenVilleClientProps';
+
+export interface IGreenVilleClientWebPartProps {
+  description: string;
+}
+
+export default class GreenVilleClientWebPart extends BaseClientSideWebPart<IGreenVilleClientWebPartProps> {
+
+  private _isDarkTheme: boolean = false;
+  private _environmentMessage: string = '';
+
+  public render(): void {
+    const element: React.ReactElement<IGreenVilleClientProps> = React.createElement(
+      GreenVilleClient,
+      {}
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onInit(): Promise<void> {
+    return super.onInit();
+  }
+
+  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+    if (!currentTheme) {
+      return;
+    }
+
+    this._isDarkTheme = !!currentTheme.isInverted;
+    const { semanticColors } = currentTheme;
+
+    if (semanticColors) {
+      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || '');
+      this.domElement.style.setProperty('--link', semanticColors.link || '');
+      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || '');
+    }
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+}
---
+++ src/webparts/greenVilleClient/components/GreenVilleClient.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import styles from './GreenVilleClient.module.scss';
+import type { IGreenVilleClientProps } from './IGreenVilleClientProps';
+
+export default class GreenVilleClient extends React.Component<IGreenVilleClientProps> {
+  public render(): React.ReactElement<IGreenVilleClientProps> {
+    const {
+    } = this.props;
+
+    return (
+      <>
+      Hello Green  Ville</>
+    );
+  }
+}
